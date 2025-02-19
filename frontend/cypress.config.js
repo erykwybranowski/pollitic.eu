27492ccbfe7e6a5d128e82013b1a32f80f18a27d
erykw <erykw@gmail.com>
2025-02-19 03:38:23 +0100

    Frontend tests fix

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://localhost',
+    baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/integration/**/*.spec.{js,ts}',
     supportFile: false
   }
